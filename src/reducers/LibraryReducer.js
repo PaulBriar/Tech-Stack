5e3b25be1d2b01ed13ef24b458155a7e4a2d9670
paulbriar <pbriar@live.com>
2019-03-04 21:37:59 -0400

    Finished wiring up Redux

--- src/reducers/LibraryReducer.js
+++ src/reducers/LibraryReducer.js
@@ -1,9 +1,14 @@
-export default async () => {
-  try {
-    const res = await fetch('http://127.0.0.1:3001/techstack')
-    const resJson = await res.json();
-    console.log(resJson);
-  } catch (err) {
-    console.log(err);
-  }
-};
+import data from './LibraryList.json';
+
+export default () => data;
+
+
+// export default async () => {
+//   try {
+//     const res = await fetch('http://127.0.0.1:3001/techstack')
+//     const resJson = await res.json();
+//     console.log(resJson);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
