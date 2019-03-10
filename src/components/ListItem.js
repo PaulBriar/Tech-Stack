fc0e32d27deae301b81e392d03c7e518b198d49d
paulbriar <pbriar@live.com>
2019-03-10 11:30:21 -0300

    Added spring, finished v1

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,17 +1,39 @@
 import React from 'react';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { Text, TouchableWithoutFeedback, View, LayoutAnimation, UIManager } from 'react-native';
 import { connect } from 'react-redux';
 import CardSection from './CardSection';
 import * as actions from '../actions';
 
+UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
+
 class ListItem extends React.Component {
+
+  renderDescription() {
+    const { library, expanded } = this.props;
+    const { descStyle } = styles;
+
+    if (expanded) {
+      return (
+        <CardSection>
+          <Text style={ descStyle }>
+            {library.item.description}
+          </Text>
+        </CardSection>
+      );
+    }
+  }
+
   render() {
     const { titleStyle } = styles;
     const { id, title } = this.props.library.item;
-    console.log(this.props);
+
     return (
       <TouchableWithoutFeedback
-        onPress={() => this.props.selectLibrary(id)}
+        onPress={() => { 
+          this.props.selectLibrary(id), 
+          LayoutAnimation.spring()
+          }
+        }
       >
         <View>
           <CardSection>
@@ -19,6 +41,7 @@ class ListItem extends React.Component {
               {title}
             </Text>
           </CardSection>
+          {this.renderDescription()}
         </View>
       </TouchableWithoutFeedback>
     );
@@ -29,7 +52,18 @@ const styles = {
   titleStyle: {
     fontSize: 18,
     paddingLeft: 15,
+  },
+  descStyle: {
+    flex: 1,
+    paddingLeft: 10,
+    paddingRight: 10
   }
 }
 
-export default connect(null, actions)(ListItem);
+const mapStateToProps = (state, ownProps) => {
+  const expanded = ownProps.library.item.id === state.selectedLibraryId;
+
+  return { expanded };
+}
+
+export default connect(mapStateToProps, actions)(ListItem);
